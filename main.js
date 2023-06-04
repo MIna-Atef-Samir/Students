b7e31ba539de03a0ff4ef098f176cd04de2080c1
Mina Atef <mina2000atef@gmail.com>
2023-06-04 17:25:11 +0300

    students array

---
+++ main.js
@@ -0,0 +1,54 @@
+let tasksArray = [
+    ["Nathalie Nader Nabil", "Task 01", "Option 2"],
+    ["Youssef Mohamed Ahmed Mohamed Youssef", "Task 01", "Option 1"],
+    ["Salma Nasreldin", "Task 01", "Option 1"],
+    ["Engy Mostafa", "Task 01", "Option 1"],
+    ["Engy Mostafa", "Task 01", "Option 1"],
+    ["Engy ahmed mostafa ", "Task 01", "Option 1"],
+    ["Abdelhay Nader Abdelhay Abozayed", "Task 01", "Option 1"],
+    ["Abdelrahman Shemies", "Task 01", "Option 1"],
+    ["Alaa Ahmed", "Task 01", "Option 2"],
+    ["Youssef Fathy Mahmoud", "Task 01", "Option 1"],
+    ["Mark Bassem", "Task 01", "Option 1"],
+    ["Anas Ahmed", "Task 01", "Option 1"],
+    ["Adham Hesham", "Task 01", "Option 1"],
+    ["Mohamed Ahmed Fahmi", "Task 01", "Option 1"],
+    ["rola wafi", "Task 01", "Option 1"],
+    ["Moataz Youssef", "Task 01", "Option 2"],
+    ["Ahmad Salama", "Task 01", "Option 1"],
+    ["Mohamed Ahmed Fahmi", "Task 01", "Option 1"],
+    ["Ahmad Salama Abdelaziz", "Task 01", "Option 2"],
+    ["Kareem Ramzi El-Tahlawi", "Task 01", "Option 1"],
+    ["Alaa Ahmed", "Task 01", "Option 2"],
+    ["rola wafi", "Task 01", "Option 2"],
+    ["Mohamed Fahmi", "Task 01", "Option 1"],
+    ["Mohamed Fahmi", "Task 01", "Option 2"],
+    ["Alaa Ahmed", "Task 01", "Option 2"],
+    ["Abdelrahman Shemies", "Task 01", "Option 1"],
+    ["Nathalie Nader", "Task 01", "Option 1"],
+    ["Mariam Ahmed", "Task 01", "Option 1"],
+  ];
+  
+  
+  
+  let arr = [];
+  let ul = document.getElementById('show')
+  
+  const getNames = function(){
+    for(let i =0; i < tasksArray.length; i++){
+        if(arr.indexOf(tasksArray[i][0]) == -1){ arr.push(tasksArray[i][0])}else console.log(`${i+1}- This Name was already added : ${tasksArray[i][0]}`);
+        // arr.push(tasksArray[i][0])
+    }
+    render()
+    return arr
+}
+const render = ()=>{
+    ul.innerHTML = ""
+    for(let i =0; i< arr.length; i++){
+        ul.innerHTML += `<li>${arr[i]}</li> <span><button onClick="remove(${i})">Delete</button></span>`
+    }
+}
+const remove = (i)=>{
+arr.splice(i,1);
+render()
+}
